322fd701dfd4dfdc32da27f6d64f81cf25acbfef
Swapnil Tamrakar <swappy208@gmail.com>
2017-11-23 14:24:43 -0500

    update

--- index.js
+++ index.js
@@ -11,18 +11,34 @@ var transporter = nodemailer.createTransport(
     }
 );
 
-var mailOptions = {
-    from: 'Awesome@gmail.com',
-    to: 'swappy208@gmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-  };
+var mylist = ['Hi. You are cute.', 'Hi Cutie. I am thinking of you.', 'Hello. What up?', 'Hi boy.'];
 
+function randomselect(mylist)
+{
+    var max = mylist.length-1;
+    var min = 0;
+    var num = Math.floor(Math.random()*(max-min+1));
+    console.log(num);
+    return mylist[num];
+}
 
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-});
+setInterval(function() {
+    
+    var mailOptions = {
+        from: 'Awesome@gmail.com',
+        to: 'swappy208@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: randomselect(mylist)
+      };
+
+    console.log(mailOptions);
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+
+}, 60 * 1000);
